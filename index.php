8ba284528e58b395be4ac3c228d3587c9264a402
zwiora <zofia.wiora@gmail.com>
2020-06-03 10:47:56 +0200

    delete unnecessary links

--- index.php
+++ index.php
@@ -6,17 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ad libitum</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="style/main.css">
 </head>
 <body>
 
 <?php
 
-require_once "element/header.php";
+require_once "elements/header.php";
 
-require_once "connect.php";
+require_once "script/connect.php";
 
 $connection = new mysqli($host, $dbUser, $dbPassword, $dbName);
 
@@ -35,7 +33,7 @@ if (!$connection) {
                 <a href="discover.php?instr=0&dif=0">Discover</a>
             </figure>
             <section class="most_popular">
-                <h2>Most popular<?= $_SESSION['x'] ?></h2>
+                <h2>Most popular</h2>
                 <article>
                     <?php
 
@@ -112,19 +110,10 @@ if (!$connection) {
 }
 
 
-require_once "element/footer.php";
+require_once "elements/footer.php";
 
 ?>
 
 <script src="https://kit.fontawesome.com/d189ad460d.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
 </body>
 </html>
